d5781730483715878bc24b958ac26eac05343bc5
estgcmu <joao231990@gmail.com>
2016-12-15 03:20:01 +0000

    Link para activity perguntas

--- app/src/main/java/com/example/joaosoares/quizdroid/MainActivity.java
+++ app/src/main/java/com/example/joaosoares/quizdroid/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.joaosoares.quizdroid;
 
-import android.support.v7.app.AlertDialog;
+import android.content.Intent;
+//import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,15 +13,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    //iniciar nova activity
+    public void pergunta(View view) {
+        Intent i = new Intent(this, pergunta.class);
+        startActivity(i);
 
-    public void alertaNormal (View view){
+        }
+
+        //if(v.getId() == R.id.ver_button)
+        //{
+        //   Intent i = new Intent(MainActivity.this, Ver.class);
+        //  startActivity(i);
+
+        //}
+
+
+
+        //public void alertaNormal (View view){
 
         //mostrar caixa de mensagem
-        AlertDialog alertDialog;
-        alertDialog = new AlertDialog.Builder(this).create();
-        alertDialog.setTitle("Funcionou");
-        alertDialog.setMessage("Botão Alerta 1");
-        alertDialog.show();
+        //AlertDialog alertDialog;
+        //alertDialog = new AlertDialog.Builder(this).create();
+        //alertDialog.setTitle("Funcionou");
+        //alertDialog.setMessage("Botão Alerta 1");
+        //alertDialog.show();
+
+        //}
+        /** Called when the user clicks the Send button */
+
 
     }
-}
+
