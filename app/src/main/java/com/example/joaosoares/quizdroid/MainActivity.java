a84e5282bd54fea1804912eeaf83b5c281901786
estgcmu <joao231990@gmail.com>
2016-12-16 16:31:32 +0000

    Criação da ligação á activity com fragment

--- app/src/main/java/com/example/joaosoares/quizdroid/MainActivity.java
+++ app/src/main/java/com/example/joaosoares/quizdroid/MainActivity.java
@@ -5,20 +5,34 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    //iniciar nova activity
-    public void pergunta(View view) {
-        Intent i = new Intent(this, Pergunta_activity.class);
-        startActivity(i);
 
-        }
+
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, Pergunta_activity.class);
+        //EditText editText = (EditText) findViewById(R.id.edit_message);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
 
         //if(v.getId() == R.id.ver_button)
         //{
--- app/src/main/java/com/example/joaosoares/quizdroid/Pergunta_fragment.java
+++ app/src/main/java/com/example/joaosoares/quizdroid/Pergunta_fragment.java
@@ -1,70 +1,26 @@
 package com.example.joaosoares.quizdroid;
 
-import android.content.Context;
-import android.net.Uri;
+
+
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
+        import android.support.annotation.Nullable;
+        import android.support.v4.app.Fragment;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
 
-
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link Pergunta_fragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link Pergunta_fragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class Pergunta_fragment extends Fragment {
 
-    private TextView pergunta_fragment;
-    private TextView texto_detalhe_opcao1;
-    private TextView texto_detalhe_opcao2;
-    private TextView texto_detalhe_opcao3;
-    private TextView texto_detalhe_opcao4;
-
-    private Pergunta mPergunta;
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,	Bundle savedInstanceState) {
-
-        View mContentView = inflater.inflate(R.layout.fragment_pergunta_fragment, container, false);
-
-        pergunta_fragment = (TextView) mContentView.findViewById(R.id.pergunta_fragment);
-        texto_detalhe_opcao1 = (TextView) mContentView.findViewById(R.id.texto_detalhe_opcao1);
-        texto_detalhe_opcao2 = (TextView) mContentView.findViewById(R.id.texto_detalhe_opcao2);
-        texto_detalhe_opcao2 = (TextView) mContentView.findViewById(R.id.texto_detalhe_opcao2);
-        texto_detalhe_opcao3 = (TextView) mContentView.findViewById(R.id.texto_detalhe_opcao3);
-        texto_detalhe_opcao4 = (TextView) mContentView.findViewById(R.id.texto_detalhe_opcao4);
-
-        return mContentView;
+    public static Pergunta_fragment newInstance() {
+        return new Pergunta_fragment();
     }
 
-  //  @Override
-  //  public void onResume() {
-  //      super.onResume();
-  //      updatePergunta();
-  //  }
+    public Pergunta_fragment() {
+    }
 
-  //  public void setPerson(Pergunta p) {
-  //      this.mPergunta = p;
-  //      updatePergunta();
-  //  }
-
-  //  public void updatePergunta() {
-  //      if(pergunta_fragment != null && texto_detalhe_opcao1 != null && texto_detalhe_opcao2 != null && texto_detalhe_opcao3 != null && texto_detalhe_opcao4 != null && mPergunta != null) {
-            //txtPersonId.setText(mPerson.getId() + "");
-  //          pergunta_fragment.setText(mPergunta.getQuestao());
-  //          texto_detalhe_opcao1.setText(mPergunta.getPrimeiraOpcao());
-    //        texto_detalhe_opcao2.setText(mPergunta.getSegundaOpcao());
-      //      texto_detalhe_opcao3.setText(mPergunta.getTerceiraOpcao());
-        //    texto_detalhe_opcao4.setText(mPergunta.getQuartaOpcao());
-
-//        }
-  //  }
-
-
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_pergunta_fragment, container, false);
+    }
 }
